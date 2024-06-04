5c4049935300c275487b50ed85ffa2819da1d807
Fahad Kiani <fjkiani@icloud.com>
2024-06-04 19:14:29 -0400

    Resolved merge conflicts between main and master

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,11 +2,9 @@ import React from "react";
 
 const Button = ({ styles }) => (
   <button type="button" className={`py-4 px-6 font-poppins font-medium text-[18px] text-primary bg-blue-gradient rounded-[10px] outline-none ${styles}`}>
-<<<<<<< HEAD
   <a href="https://www.fahadkiani.com/contact" target="_blank" rel="noreferrer"> Get Started</a>
 =======
   <a href="https://www.fahadkiani.com" target="_blank" rel="noreferrer"> Get Started</a>
->>>>>>> origin/main
   </button>
 );
 
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,6 @@
 import styles from "../style";
 import { logo } from "../assets";
 import { footerLinks, socialMedia } from "../constants";
-<<<<<<< HEAD
 import demoVideo from "../assets/demo.mp4"; // Import the video file
 
 const Footer = () => {
@@ -41,33 +40,5 @@ const Footer = () => {
     </section>
   );
 };
-=======
-
-const Footer = () => (
-  <section className={`${styles.flexCenter} ${styles.paddingY} flex-col`}>
-    {/* Embed YouTube Video Start */}
-    <div className="video-responsive">
-      <iframe
-        width="560"
-        height="315"
-        src="https://www.youtube.com/embed/62FYX-CFVV4"
-        title="YouTube video player"
-        frameborder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        allowfullscreen
-      ></iframe>
-    </div>
-    {/* Embed YouTube Video End */}
-    
-    <div className={`${styles.flexStart} md:flex-row flex-col mb-8 w-full`}>
-      ...
-    </div>
-
-    <div className="w-full flex justify-between items-center md:flex-row flex-col pt-6 border-t-[1px] border-t-[#3F3E45]">
-      ...
-    </div>
-  </section>
-);
->>>>>>> origin/main
 
 export default Footer;
